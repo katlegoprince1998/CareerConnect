e709f0f10ab02d8b01e9178a37ffedc3e873dc26
katlegoprince1998 <princekatlego357@gmail.com>
2024-11-09 18:24:25 +0200

    Config server and spring cloud

--- GateWay/src/main/java/com/absolute/code/weirdo/GateWay/GateWayApplication.java
+++ GateWay/src/main/java/com/absolute/code/weirdo/GateWay/GateWayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GateWayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GateWayApplication.class, args);
 	}
